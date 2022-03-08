5a275d3103631a10fb92fee305ce565e4156f231
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-03-08 17:59:11 +0200

    add job views and url

--- job/models.py
+++ job/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -16,6 +16,13 @@ class Job(models.Model):
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
