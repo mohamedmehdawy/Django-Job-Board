e7721791336117e865299e7ae80b4be51f5653ae
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-03-29 21:26:59 +0200

    add filter to jobs

--- job/views.py
+++ job/views.py
@@ -1,21 +1,23 @@
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
 from .models import Job
 from .forms import ApplyForm, AddJobForm
+from .filters import JobFilter
 
 # Create your views here.
 
 def job_list(request):
-    job_list = Job.objects.all()
-    
-    paginator = Paginator(job_list, 5)
+    filter = JobFilter(request.GET, queryset=Job.objects.all())
+    paginator = Paginator(filter.qs, 5)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-
+    
     context = {
-        'jobs': page_obj
+        'jobs': page_obj,
+        'filter': filter.form
     }
     return render(request, 'job/job_list.html', context)
 
@@ -37,6 +39,7 @@ def job_detail(request, slug):
     }
     return render(request, 'job/job_detail.html', context)  
 
+@login_required
 def add_job(request):
     
     if request.method == "POST":
